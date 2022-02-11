e2bbd78939ea9a4621dd3475d3187628f9004c8a
Gabriel Richer <e1934026@cmaisonneuve.qc.ca>
2022-02-11 16:06:36 -0500

    ex1

--- footer.php
+++ footer.php
@@ -2,6 +2,9 @@
     <h2 class="footer__titre">Le beau footer</h2>
     <p class="footer__presentation">Un site web fait par une élève du TIM à l'aide de Wordpress.</p>
     <h3 class="footer__author">Fait par Florence Lemieux Cayer</h3>
+    <?php wp_nav_menu(array("menu" => "simple",
+                            "container" => "nav",
+                            "container_class" =>"footer")); ?>
 </footer>
 </body>
 
